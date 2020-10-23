2c47ea62b189edcf4245e20404208fd9d2e80e5f
Michal Pietrzak <michal.pietrzak18@gmail.com>
2020-10-23 02:35:28 +0200

    added tableViewCell + fixed getting car images from folder + connecting tableView with tableViewCell

--- CarListApp/CarListApp/Controllers/TableViewController.swift
+++ CarListApp/CarListApp/Controllers/TableViewController.swift
@@ -8,21 +8,34 @@
 import UIKit
 
 class TableViewController: UITableViewController {
-        
+    
+    var cars = [String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
 
+        for item in items {
+            if item.hasSuffix("3x.png") {
+                cars.append(item)
+            }
+        }
+        print(cars)
+        
+        tableView.register(UINib(nibName: "CarTableViewCell", bundle: nil), forCellReuseIdentifier: "CarTableViewCell")
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return cars.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CarTableViewCell", for: indexPath) as! CarTableViewCell
+        
+        let carName = cars[indexPath.row]
         return cell
     }
     
